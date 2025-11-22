06b662e346001a89a68a3087f610a8dd5e05da43
DiegoRodrigues06 <diegodash06@gmail.com>
2025-11-22 13:34:34 -0300

    Revert "first migration, downgraded .NET sdk to 8.0"
    This reverts commit 28568c6b8f3519936dae1d8c602f58101a241d42.

--- backend/Program.cs
+++ backend/Program.cs
@@ -4,9 +4,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
@@ -21,9 +20,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
+
 app.UseHttpsRedirection();
 
 var summaries = new[]
--- backend/models/Users.cs
+++ backend/models/Users.cs
@@ -2,7 +2,7 @@ namespace backend.models
 {
     public class Users
 {
-    public int Id { get; set; } // tem q ser public se não o ef não reconhece
+    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
 
     public List<Expense> Expenses { get; set; } 
