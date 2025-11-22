28568c6b8f3519936dae1d8c602f58101a241d42
DiegoRodrigues06 <diegodash06@gmail.com>
2025-11-22 13:29:17 -0300

    first migration, downgraded .NET sdk to 8.0

--- backend/models/Expense.cs
+++ backend/models/Expense.cs
@@ -4,12 +4,12 @@ namespace backend.models
 {
     public class Expense
     {
-        private int Id { get; set; }
-        private string Description { get; set; }
-        private float Value { get; set; }
-        private DateTime Date { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public float Value { get; set; }
+        public DateTime Date { get; set; }
 
-        private int UsersID { get; set; }
-        private Users User { get; set; }
+        public int UsersID { get; set; }
+        public Users User { get; set; }
     }
 }
